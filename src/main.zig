546a0c52b4e51ce9e5d23e680fbdf8539f222fc8
12ball <pub@janie.page>
2025-07-11 19:38:00 -0400

    More zig

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+const Point = struct { x: f64, y: f64 };
+
 pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("here's a trig answer: {d:.3}\n", .{std.math.tan(std.math.pi * 0.45)});
 }
